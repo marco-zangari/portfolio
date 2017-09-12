165facb6fa2fbeba81bbb7733be2b4b126208659
Marco Zangari <mjzangari@gmail.com>
2017-09-11 18:44:19 -0700

    added constructor function and properties to .js

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
 'use strict';
+
+function Projects (title,date) {
+  this.title = title;
+  this.url = url;
+  this.contributor = contributor;
+  this.image = image;
+  this.date = date;
+
+}
+
+var = new Projects ();
+
+function WorkHistory (title,workPlace) {
+  this.title = title;
+  this.workPlace = workPlace;
+  this.startDate = startDate;
+  this.endDate = endDate;
+  this.responsibility = responsibility;
+}
+
+var = new WorkHistory ();
+
+function Education (institution,place,degree) {
+  this.institution = institution;
+  this.place = place;
+  this.degree = degree;
+}
+
+var = new Education ();
